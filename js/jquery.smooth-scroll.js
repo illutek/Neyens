50b5100fa6ebf819c9243cd3fa50a4f8cd24706e
Vandenborne Stefan <stefan@illutek.be>
2015-11-04 23:16:09 +0100

    JavaScript closures on js files

--- js/jquery.smooth-scroll.js
+++ js/jquery.smooth-scroll.js
@@ -1,4 +1,11 @@
-$(function() {
+/**
+ * Created by stefan on 4/10/2015.
+ * It's best practice to wrap your code in a closure. A closure is nothing more
+ * than a function that helps limit the scope of variables so you don't
+ * accidentally overwrite global variables.
+ * https://www.drupal.org/node/171213
+ */
+jQuery(document).ready(function($) {
 	$('a[href*=#]:not([href=#])').click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 			var target = $(this.hash);
