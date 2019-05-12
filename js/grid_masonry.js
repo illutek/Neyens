55c7428e42658d022a5f931f68b7e9ff12991acd
illutek <stefanvandenborne@gmail.com>
2019-05-12 22:59:01 +0200

    added gulp and reset to new git repo

---
+++ js/grid_masonry.js
@@ -0,0 +1,9 @@
+/**
+ * Created by stefan on 22/10/2015.
+ */
+$('.grid').masonry({
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 195
+});
+
---
+++ templates/old_page.tpl.php
@@ -0,0 +1,279 @@
+<div class="fullscreen-bg">
+    <video loop muted autoplay poster="<?php print base_path() . path_to_theme(); ?>/video/jasmine_flower_127/snapshot-min.PNG" class="fullscreen-bg__video">
+        <source src="<?php print base_path() . path_to_theme(); ?>/video/jasmine_flower_127/Jasmine_flower.mp4" type="video/mp4">
+        <source src="<?php print base_path() . path_to_theme(); ?>/video/jasmine_flower_127/Jasmine_flower.ogv" type="video/ogv">
+        <source src="<?php print base_path() . path_to_theme(); ?>/video/jasmine_flower_127/Jasmine_flower.ogg" type="video/ogg">
+        <source src="<?php print base_path() . path_to_theme(); ?>/video/jasmine_flower_127/Jasmine_flower.webmsd.webm" type="video/webm">
+    </video>
+</div>
+<!-- navigatie fixed top -->
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="#home">Home <i class="fa fa-home"></i></a></li>
+                <li><a href="#prive-tuinen">Prive tuinen en parken <i class="fa fa-map-pin"></i></a></li>
+                <li><a href="#bedrijven">Bedrijven <i class="fa fa-industry"></i></a></li>
+                <li><a href="#steden">Steden en gemeente <i class="fa fa-university"></i></a></li>
+                <li><a href="#realisaties">Realisaties <i class="fa fa-picture-o"></i></a></li>
+                <li><a href="#contact">Contact <i class="fa fa-envelope"></i></a></li>
+            </ul>
+            <!-- <ul class="nav navbar-nav navbar-right">
+                <li><a href="#acties">Acties <i class="fa fa-info"></i></a></li>
+            </ul> -->
+
+        </div>
+    </div>
+</nav>
+<div id="home"></div>
+<header class="container">
+    <div class="row">
+        <div class="experience">
+            <a href="#home">
+                <figure><img src="<?php print base_path() . path_to_theme(); ?>/images/twentyfive_year_active-stroke-200-min.png"></figure>
+            </a>
+        </div>
+        <div id="sticky-anchor"></div>
+        <!-- als deze de top vd page bereikt wordt .experience sticky -->
+        <div class="site-name">
+            NEYENS - Tuinaanleg - Tuinonderhoud
+        </div>
+    </div>
+    <div class="actie-wrap">
+        <a href="#acties">
+            <div class="acties"></div>
+        </a>
+    </div>
+</header>
+
+<section class="center">
+    <div class="main-wrap-top">
+        <div class="row green-canvas">
+        <!-- dit stuk code om de main content te printen zou moeten verdwijnen op de front page -->
+
+            <article class="col-md-12 content"><!-- main content -->
+                <span>
+                    <?php print $messages; ?>
+                    <div id="main-wrapper">
+                        <div id="main" class="clearfix">
+                            <div id="content" class="column">
+                                <div class="section">
+                                    <a id="main-content"></a>
+                                    <?php print render($title_prefix); ?>
+                                    <?php if ($title): ?>
+                                        <h1 class="title" id="page-title"><?php print $title; ?></h1>
+                                    <?php endif; ?>
+                                    <?php print render($title_suffix); ?>
+                                    <?php if ($tabs): ?>
+                                        <div class="tabs"><?php print render($tabs); ?></div>
+                                    <?php endif; ?>
+                                    <?php print render($page['help']); ?>
+                                    <?php if ($action_links): ?>
+                                        <ul class="action-links"><?php print render($action_links); ?></ul>
+                                    <?php endif; ?>
+
+                                    <?php if(!empty($page['content'])): ?>
+                                    <?php if(drupal_is_front_page()) {
+                                        unset($page['content']['system_main']['default_message']);
+                                    }
+                                    ?>
+                                        <?php print render($page['content']); ?>
+
+                                    <?php endif; ?>
+
+                                </div>
+                            </div>
+                            <!-- /.section, /#content -->
+                </span>
+            </article>
+
+        </div>
+    </div>
+</section>
+<section class="center">
+    <div class="main-wrap-top">
+        <div class="row green-canvas">
+            <?php print render($page['welkom']); ?>
+        </div>
+    </div>
+</section>
+<section class="center" id="prive-tuinen">
+    <div class="main-wrap">
+        <div class="row green-canvas">
+            <figure class="col-md-4 image-wrap"><img src="../images/prive.jpg"></figure>
+            <article class="col-md-8 content">
+                <span>
+                    <h2>Privé tuinen en parken</h2>
+                <p>Aanleg en Renovaties van bestaande tuinen.
+                    Tuinaanleg van nieuwbouw woningen, het leveren en plaatsen van alle soorten
+                    verhardingen, leveren en plaatsen van tuinverlichtingen, alle soorten
+                    afsluitingen gaande van draad, hout, betonproducten enz.
+                    Planten van sierplanten, bomen en vaste planten zaiien van alle type's
+                    grasperken inclusief kunstgrassen.
+                    Alle grondwerken zoals leveren en verspreiden van zwarte grond nivelleren
+                    van terreinen met laadgraafcombinatie of bobcat voor kleinere ruimtes.
+                    Plaatsen van alle leidingen zoals water, stroom, afvoer, telefoon,
+                    met minigraver alle kleine grondwerken.</p>
+                    <p>Alle tuinwerken kunnen we apart uitvoeren voor de doe-het-zelvers en
+                        voor ieders budget.</p>
+                    <p>Tevens het plaatsen van containers voor groenafval en tuinresten.</p>
+                </span>
+            </article>
+        </div>
+    </div>
+</section>
+
+<section class="center" id="bedrijven">
+    <div class="main-wrap">
+        <div class="row green-canvas">
+            <figure class="col-md-4 image-wrap"><img src="../images/bedrijven.jpg"></figure>
+            <div class="col-md-8 content">
+                <span>
+                <h2>Bedrijven en Industrieparken</h2>
+                <p>Het aanleggen van wegen en parkings op industrie terreinen en rondom gebouwen.
+                    Het leveren en plaatsen van omheiningen en poorten, afsluitingen in verschillende
+                    materialen zoals draad, hout, betonproducten.</p>
+                    <p>Plaatsen van rioleringen en afvoerputten. Leveren en plaatsen van verlichtingen.
+                        Het leveren en planten van groenbeplantingen sierheester en bomen.</p>
+                    <p>Het aanleggen van grasperken.</p>
+                    <p>Wekelijks onderhoud van uw bedrijfsterrein zowal grasmatten als beplantingen
+                        kan door ons uitgevoerd worden.Tevens kan u op ons rekenen voor het
+                        winteronderhoud van uw wegen en parkings het sneeuwvrij maken en het
+                        strooien van zout.</p>
+                    <p>Ook wanneer uw bedrijf niet door ons is aangelegd kan u steeds op ons
+                        beroep doen voor de nodige onderhoudswerken en winterdiensten.</p>
+                </span>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="center" id="steden">
+    <div class="main-wrap">
+        <div class="row green-canvas">
+            <figure class="col-md-4 image-wrap"><img src="../images/steden.jpg"></figure>
+            <div class="col-md-8 content">
+                <span>
+                <h2>Steden en Gemeenten</h2>
+                <p>Het aanleggen van openbaar groen van parken,het realiseren van
+                    speelpleinen zowel de grasperken als zandbakken tot het leveren en
+                    plaatsen van alle gekeurde speeltoestellen.het aanleggen van sportpleinen
+                    zowel in grasmatten als het kunstgras ,tevens kunnen wij het wekelijks
+                    maaien van uw sportvelden uitvoeren eventueel met opvang van het gras
+                    ofwel het mulchen.</p>
+                    <p>Het aanplanten van bomen en sierheesters langs wegen en waterwegen.
+                        het onderhoud van fietspaden zowel het zomeronderhoud
+                        (borstelen en afranden) als het winteronderhoud (sneeuwvrij maken en
+                        strooien van zout of pekel) kunnen wij uitvoeren vraag hiervoor een
+                        passende offertte aan. Het plaatsen van inritten en voetpaden.</p>
+                    <p>De complete aanleg en groenonderhoudswerken van speelpleinen, kerkhoven,
+                        sportterreinen en aan alle openbare gebouwen.</p>
+                </span>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section class="center" id="realisaties">
+    <div class="main-wrap">
+        <div class="green-canvas">
+            <article class="col-md-12 content">
+                <h2>Realisatie</h2>
+
+                <div class="img_realisaties"></div>
+            </article>
+        </div>
+    </div>
+</section>
+
+<section class="center" id="contact">
+    <div class="main-wrap">
+        <div class="row yellow-canvas">
+            <div class="col-md-5 mention">
+                <p>Contacteer ons via dit formulier voor al je vragen of voor een offerte</p>
+
+                <div class="arrow-right">
+                    <i class="fa fa-arrow-right arrow"></i>
+                </div>
+                <div class="arrow-down">
+                    <i class="fa fa-arrow-down arrow"></i>
+                </div>
+
+            </div>
+            <div class="col-md-6 col-md-offset-1 form">
+                <form>
+                    <label>Jouw naam</label> <input type="text" size="40" name="name"><br>
+                    <label>e-mail</label> <input type="email" size="40" name="email"><br>
+                    <label>Bericht</label> <textarea name="bericht" row="8" cols="40"></textarea><br>
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="center" id="acties">
+    <div class="main-wrap-acties">
+        <div class="row yellow-canvas">
+            <span>
+                <h2>Aktie : Korting van 10% op uw huidig onderhoudscontract - Offertte</h2>
+            <p>Aktie van 10 % korting op uw huidig groenonderhoudscontract.</p>
+            <p>Hebt u een lagere offerte dan de onze, dan verlagen wij onze prijs - Zodat wij deze steeds aan de beste
+                voorwaarden kunnen voldoen!
+                Nieuw : Plaatsen van Citycontainers voor groenafval max 10m³ aan voordelige prijzen.
+                Plaatsen van Citycontainers voor alle soorten afval, vanaf 6 m³ VRAAG NAAR DE PLAATSINGSVOORWAARDEN</p>
+            </span>
+        </div>
+    </div>
+</section>
+<footer>
+    <div class="footer-wrap">
+        <div class="row green-canvas footer-canvas">
+            <div class="col-sm-6 address">
+                <div class="phone-screen margin-bottom">
+                    <i class="fa fa-phone"></i> 003211/63.35.28
+                </div>
+                <div class="phone-mobile margin-bottom">
+                    <a href="tel:+3211633528" class="btn btn-info">
+                        <i class="fa fa-phone"></i> 003211/63.35.28
+                    </a>
+                </div>
+                <div class="phone-screen margin-bottom">
+                    <i class="fa fa-mobile"></i> 0495/30.35.28
+                </div>
+                <div class="phone-mobile margin-bottom">
+                    <a href="tel:+32495303528" class="btn btn-info">
+                        <i class="fa fa-mobile"></i> 0495/30.35.28
+                    </a>
+                </div>
+                <div class="mail margin-bottom">
+                    <i class="fa fa-envelope"></i>
+                    <script type="text/javascript">
+                        document.write("<a href=\"mail" + "to:" + new Array("info", "neyens.be").join("@") + "\">" + "info@neyens.be" + "</" + "a>");
+                    </script>
+                </div>
+                <div class="btw margin-bottom">
+                    BE 0705.544.643
+                </div>
+                <div>
+                    <p>Postadres :<br>Weyerstraat 325<br>3850 Kozen België</p>
+                </div>
+            </div>
+            <!-- end address -->
+
+            <div class="col-sm-6">
+                <img src="../images/logo_small.jpg">
+                Neyens | &copy; 2015
+            </div>
+        </div>
+    </div>
+</footer>
